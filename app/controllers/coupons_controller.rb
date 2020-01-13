5799691e6c9085da1a7e04595606f011f83decbc
sydnee510 <sbsydnee161416@gmail.com>
2020-01-13 08:37:51 -0800

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,11 +6,12 @@ class CouponsController < ApplicationController
         @coupon = Coupon.find(params[:id])
     end
     def new 
+        @coupon = Coupon.new
     end
     def create 
         @coupon = Coupon.new 
-        @coupon[:coupon_code] = params[:coupon][:coupon_code]
-        @coupon[:store] = params[:coupon][:store]
+        @coupon.coupon_code = params[:coupon][:coupon_code]
+        @coupon.store = params[:coupon][:store]
         @coupon.save 
         redirect_to coupon_path(@coupon)
     end
