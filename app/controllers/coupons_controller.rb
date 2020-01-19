5f73023c8c5338a3a7784085fb974294dcbc22b0
kierra <51520037+kierrakay@users.noreply.github.com>
2020-01-19 17:27:24 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,4 @@
+
 class CouponsController < ApplicationController
  
     def index
@@ -12,7 +13,8 @@ class CouponsController < ApplicationController
       end
     
       def create
-        @coupon = Coupon.create(store: params[:store],coupon_code: params[:coupon_code])
+        # binding.pry
+        @coupon = Coupon.create(store: params[:coupon][:store], coupon_code: params[:coupon][:coupon_code])
         redirect_to coupon_path(@coupon)
       end
 
